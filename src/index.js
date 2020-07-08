99d8acb1ef23cf6c1410d1e98b53d254f0200afd
Felipe Rosa <flp.far@gmail.com>
2020-07-08 18:03:20 -0300

    Add tasks to projects

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Project, renderProject } from "./Components/ProjectCreator";
 const projectsList = []
 
 const newProjectBtn = document.querySelector('.new-project-btn');
+const newTaskBtn = document.querySelector('.new-task-btn');
 const projectsListDiv = document.querySelector('.projects-list');
 
 newProjectBtn.addEventListener('click', ()=>{
@@ -12,6 +13,20 @@ newProjectBtn.addEventListener('click', ()=>{
   const newProject = new Project(projectName);
   projectsList.push(newProject);
   projectsListDiv.appendChild(renderProject(newProject));
+})
+
+newTaskBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const taskTitle = document.getElementById('task-title').value;
+  const taskDescription = document.getElementById('task-description').value;
+  const taskDueDate = document.getElementById('task-due-date').value;
+  const taskPriority = document.getElementById('task-priority').value;
+  const taskProjectName = document.getElementById('task-project').value;
+
+  const newTask = new Task(taskTitle, taskDescription, taskDueDate, taskPriority);
+  const taskProject = projectsList.filter(item => item.name == taskProjectName)[0];
+  taskProject.addTaskToList(newTask);
 })
 
 for(let i=0; i < projectsList.length; i++) {
